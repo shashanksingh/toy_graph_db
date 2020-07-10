a7e61bef6c46ed75afd4a151e0b4ad0744e240d8
shashanksingh <shashank.personal@gmail.com>
2020-07-10 02:36:40 +0100

    one more unit test for good measure

--- tests/test_dag.py
+++ tests/test_dag.py
@@ -10,3 +10,8 @@ def test_topological_sort_with_two_nodes():
 def test_topological_sort_with_three_nodes():
     dag = Dag(3, [[0, 1], [1, 2]])
     assert dag.topological_sort() == [0, 1, 2]
+
+
+def test_topological_sort_with_four_nodes():
+    dag = Dag(4, [[1,0],[1,2],[1,3]])
+    assert dag.topological_sort() == [1, 3, 2, 0]
