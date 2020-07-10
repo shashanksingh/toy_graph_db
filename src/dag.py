401233c16a29afb6849d389beafde33cfbf0def1
shashanksingh <shashank.personal@gmail.com>
2020-07-10 02:16:21 +0100

    test for topological sort

--- src/dag.py
+++ src/dag.py
@@ -34,3 +34,11 @@ class Dag(Graph):
         while no_ingress_node:
             element = no_ingress_node.pop()
             topological_sort.append(element)
+
+            for connected_node in self.graph[element]:
+                self.ingress_count[connected_node] -= 1
+                if self.ingress_count[connected_node] == 0:
+                    no_ingress_node.append(connected_node)
+            if len(topological_sort) != self.number_of_nodes:
+                return [] #no cycles
+            return topological_sort
