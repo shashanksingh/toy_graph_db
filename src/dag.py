87bddc39e0404cf58f7a5e40582ff2e8381f0a56
shashanksingh <shashank.personal@gmail.com>
2020-07-10 02:34:57 +0100

    all the tests pass

--- src/dag.py
+++ src/dag.py
@@ -29,19 +29,18 @@ class Dag(Graph):
         no_ingress_node = [
             node
             for node in range(self.number_of_nodes)
-            if node not in self.adjacency_list
+            if node not in self.ingress_count
         ]
 
         while no_ingress_node:
             element = no_ingress_node.pop()
             topological_sort.append(element)
 
-            for connected_node in self.graph[element]:
+            for connected_node in self.adjacency_list[element]:
                 self.ingress_count[connected_node] -= 1
                 if self.ingress_count[connected_node] == 0:
                     no_ingress_node.append(connected_node)
-        print(len(topological_sort))
         if len(topological_sort) != self.number_of_nodes:
-            return [] #cycles
+                return [] #cycles
         else:
-            return topological_sort[::-1]
+            return topological_sort # why no reversed ?
--- tests/test_dag.py
+++ tests/test_dag.py
@@ -2,6 +2,11 @@ from unittest import TestCase
 from src.dag import Dag
 
 
-def test_topological_sort():
+def test_topological_sort_with_two_nodes():
     dag = Dag(2, [[0, 1]])
     assert dag.topological_sort() == [0, 1]
+
+
+def test_topological_sort_with_three_nodes():
+    dag = Dag(3, [[0, 1], [1, 2]])
+    assert dag.topological_sort() == [0, 1, 2]
