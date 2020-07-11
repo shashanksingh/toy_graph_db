2280dda21e383f350f53da46fff08600edd34e0c
shashanksingh <shashank.personal@gmail.com>
2020-07-11 02:48:36 +0100

    all theee work

--- src/main.py
+++ src/main.py
@@ -3,10 +3,14 @@
 from undirected import Undirected
 from dag import Dag
 from grid import Grid
+
+
 a = Undirected(2, [[0, 1]])
 b = Dag(2, [[0, 1]])
 c = Grid([[1, 0, 1]])
 
 print(a.has_cycles())
+print(b.topological_sort())
+print(c.count_of_connected_components())
 
 # needs to have a RPC server to load/create/read/run_functions/write objects when needs
