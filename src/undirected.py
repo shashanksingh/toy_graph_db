f243ebfbf434541d19a24a7d4e5e248c6f83869f
shashank singh <shashanksingh@users.noreply.github.com>
2020-07-12 17:49:08 +0100

    [Enhacement-9] (#10)
    * fixed return type
    
    * youtube
    
    * aadded tests
    
    * sic structure
    
    * undirected gets onion peel algorithm
    
    * bug fixed

--- src/undirected.py
+++ src/undirected.py
@@ -3,6 +3,9 @@ from typing import List
 from collections import defaultdict, deque
 from hashlib import md5
 
+MAXIMUM_NUMBER_OF_NODES_THAT_CAN_BE_CENTER = 2
+NUMBER_OF_INGRESS_CONNECTION_FOR_LEAVES = 1
+
 
 class Undirected(Graph):
     def __init__(self, number_of_nodes: int, edge_list: List[List[int]]):
@@ -26,6 +29,47 @@ class Undirected(Graph):
 
     def number_of_components(self):
         pass
+
+    def find_roots_of_minimum_height_tree(self):
+        return self.find_center()
+
+    # source : https://www.youtube.com/watch?v=nzF_9bjDzdc
+    def find_center(self) -> List[int]:
+        if not self.is_tree():
+            return []
+        if self.number_of_nodes == 1:
+            return [0]
+
+        ingress_count = [0] * self.number_of_nodes
+
+        # calculate all ingress
+        for edge_to, edge_from in self.edge_list:
+            ingress_count[edge_to] += 1
+            ingress_count[edge_from] += 1
+
+        leaves = [
+            node
+            for node in range(self.number_of_nodes)
+            if ingress_count[node] <= NUMBER_OF_INGRESS_CONNECTION_FOR_LEAVES
+        ]
+
+        count_of_nodes_touches = 0
+        while (
+            count_of_nodes_touches
+            < self.number_of_nodes - MAXIMUM_NUMBER_OF_NODES_THAT_CAN_BE_CENTER
+        ):
+            # go through all leaves
+            # peel them and make ingress for their neibour/kids -= 1
+            inner_layer_of_leaves = []
+            for leave in leaves:
+                for kid in self.adjacency_list[leave]:
+                    ingress_count[kid] -= 1
+                    if ingress_count[kid] == 1:
+                        inner_layer_of_leaves.append(kid)
+                ingress_count[leave] = 0
+            count_of_nodes_touches += len(leaves)
+            leaves = inner_layer_of_leaves
+        return leaves
 
     # source : https://leetcode.com/problems/graph-valid-tree/discuss/382112/easy-peasy-python-solution-comments
     def is_tree(self) -> bool:
--- tests/test_undirected.py
+++ tests/test_undirected.py
@@ -9,3 +9,18 @@ def test_is_it_a_valid_tree():
 def test_is_it_a_valid_tree_with_cycle():
     graph = Undirected(5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]])
     assert not graph.is_tree()
+
+
+def test_find_center_with_4_nodes():
+    graph = Undirected(4, [[1, 0], [1, 2], [1, 3]])
+    assert graph.find_center() == [1]
+
+
+def test_find_center_with_5_nodes():
+    graph = Undirected(5, [[0, 1], [0, 2], [0, 3], [0, 4]])
+    assert graph.find_center() == [0]
+
+
+def test_find_center_with_6_nodes():
+    graph = Undirected(6, [[0, 3], [1, 3], [2, 3], [4, 3], [5, 4]])
+    assert graph.find_center() == [3, 4]
