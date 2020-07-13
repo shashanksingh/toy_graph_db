0845a45626186e48da54eb0d381741e6774bcc58
shashank singh <shashanksingh@users.noreply.github.com>
2020-07-13 01:20:00 +0100

    [Enhancement -11] Undirected > Find number of components (#12)
    * fixed return type
    
    * youtube
    
    * tests addewd for number of components
    
    * number of components for undirected
    
    * find_component and number_of_component function adddeed
    
    * test
    
    * fixed singature
    
    * fixed d
    
    * fixed the test

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from src.undirected import Undirected
 from src.dag import Dag
 from src.grid import Grid
 from src.graph import Graph
+from src.tree import Tree
 
 a = Undirected(2, [[0, 1]])
 b = Dag(2, [[0, 1]])
 c = Grid([[1, 0, 1]])
 d = Graph()
+e = Tree(2, [[0, 1]])
 
 print(a.is_tree())
 print(b.topological_sort())
@@ -18,5 +20,6 @@ print()
 a.save_into_storage()
 b.save_into_storage()
 c.save_into_storage()
+e.save_into_storage()
 print(d.list_all())
 # needs to have a RPC server to load/create/read/run_functions/write objects when needs
