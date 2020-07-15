5c411e0075b3eb65f5c4069c8230b9a42bca43e2
shashanksingh <shashank.personal@gmail.com>
2020-07-15 16:50:01 +0100

    fixed mai

---
+++ main.py
@@ -0,0 +1,47 @@
+# https://levelup.gitconnected.com/understanding-grpc-a-practical-application-in-go-and-python-f3003c9158ef
+
+# imports for functionality
+from src.undirected import Undirected
+from src.dag import Dag
+from src.grid import Grid
+from src.graph import Graph
+from src.tree import Tree
+
+# imports for the server
+import grpc
+from src.generated.query_servicer_pb2_grpc import (
+    ToyGraphDBServicer,
+    add_ToyGraphDBServicer_to_server,
+)
+import logging
+from concurrent import futures
+
+PORT_EXPOSED = 8080
+
+
+class ToyGraphDBServicer(ToyGraphDBServicer):
+    def __init__(self):
+        pass
+
+    def read_graph(self, request, context):
+        pass
+
+    def create_graph(self, request, context):
+        pass
+
+    def call_functionality_in_graph(self, request, context):
+        pass
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    add_ToyGraphDBServicer_to_server(ToyGraphDBServicer(), server)
+    server.add_insecure_port(f"[::]:{PORT_EXPOSED}")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    print(f"Running on port {PORT_EXPOSED}")
+    serve()
