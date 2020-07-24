16955a2859f66870023cc0dbaaaae453345b0ef3
shashanksingh <shashank.personal@gmail.com>
2020-07-24 19:01:29 +0100

    fixing proto 	based on https://www.freecodecamp.org/news/googles-protocol-buffers-in-python/

--- clients/examples/create_grid.py
+++ clients/examples/create_grid.py
@@ -1,6 +1,11 @@
 import grpc
 from src.generated import query_servicer_pb2, query_servicer_pb2_grpc
-from src.generated.query_servicer_pb2 import Empty, ToyGraphDBRequest, Type_of_graphs, grid
+from src.generated.query_servicer_pb2 import (
+    Empty,
+    ToyGraphDBRequest,
+    Type_of_graphs,
+    grid,
+)
 
 PORT_EXPOSED = 9090
 SAMPLE_GRID = [[0, 1, 1], [0, 1, 1], [0, 1, 1]]
@@ -19,12 +24,11 @@ stub = query_servicer_pb2_grpc.ToyGraphDBStub(channel)
 # request_data.data
 # create a valid request message
 
-print(grid)
 # make the call
 response = stub.ping(Empty())
 print(response)
 
-type_of_graph = Type_of_graphs() #query_servicer_pb2.Type_of_graphs.GRID
+type_of_graph = Type_of_graphs(type=)  # query_servicer_pb2.Type_of_graphs.GRID
 grid_request_object = grid(data=query_servicer_pb2.List_of_list_of_integer(data=None))
 
 request = ToyGraphDBRequest(type=type_of_graph, grid=grid_request_object)
