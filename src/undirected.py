0305a6e8340488b97c479cad29b200cdb6a069bf
shashanksingh <shashank.personal@gmail.com>
2020-08-04 13:11:20 +0100

    undircted also gets it

--- src/undirected.py
+++ src/undirected.py
@@ -16,7 +16,7 @@ class Undirected(Graph):
         super()
 
     def __hash__(self):
-        return self.get_class_name() + md5(("").encode("utf-8")).hexdigest()
+        return hash(str(self.adjacency_list)+str(self.edge_list)+str(self.number_of_nodes))
 
     def setup_adjacency(self):
         self.adjacency_list = {x: [] for x in range(self.number_of_nodes)}
